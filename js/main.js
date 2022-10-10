63d60b63e019ecd8083d92203b351a293b343b28
CodePro69 <114254101+CodePro69@users.noreply.github.com>
2022-10-10 14:38:05 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
